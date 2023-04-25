92daa62cf6e38e727bffa199d0cf15863edd1bf5
Guillermo Kopacek <kopacek5@gmail.com>
2023-04-25 17:00:45 -0300

    prueba con el pom de mgb

--- src/main/java/com/myportfolioAP/gk/GkApplication.java
+++ src/main/java/com/myportfolioAP/gk/GkApplication.java
@@ -3,6 +3,7 @@ package com.myportfolioAP.gk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GkApplication {
 
